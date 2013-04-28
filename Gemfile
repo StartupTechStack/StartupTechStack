a89083c656333bf1d1f134d5b4d67c60997c2a03
James Solomon <solomonjames@gmail.com>
2013-04-27 23:30:28 -0400

    first api call working

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'http://rubygems.org'
 gem "sinatra", "~> 1.4.2"
 gem "mongodb", "~> 2.1.0"
 gem "thin", "~> 1.5.1"
+gem "bson_ext", "~> 1.8.5"
--- app/public/app.rb
+++ app/public/app.rb
@@ -1,2 +1,31 @@
 require 'sinatra'
+require 'json'
 require 'mongo/driver'
+
+configure do
+  set :bind, '192.168.33.98'
+end
+
+connection = Mongo::Connection.new
+db = connection.startuptechstack
+
+# Home page
+get '/' do
+  'Startup [Tech] Stack'
+end
+
+# Get all companies, and their data
+get '/companies' do
+  companies = db.companies.all
+  companies.to_json
+end
+
+# Get all funding summarized by category
+get '/funding/categories' do
+
+end
+
+# Get all funding summarized by tech
+get '/funding/tech' do
+
+end
